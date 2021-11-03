ac7f267a825d5ccb2d57322742999dbdfac7892c
M.TASHFEEN <kingtashfeen@gmail.com>
2021-11-03 23:07:27 +0500

    ep17.list rendering

---
+++ src/components/NameList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Person from './Person'
+
+function NameList() {
+    // const names = ['Bruce','Clark','Diana']
+    // const namelist = names.map( name => <h1>{name}</h1>)
+    // return <div>{namelist}</div> 
+
+    const persons = [
+        {
+            id:1,
+            name:'qwe',
+            age:50,
+        },
+        {
+            id:2,
+            name:'asxc',
+            age:20,
+        }
+    ]
+
+    const personList = persons.map( person => (<Person person={person}></Person>) ) 
+    return <div>{personList}</div> 
+}
+
+export default NameList
+
+// name.propertyname if the object has properry and values
