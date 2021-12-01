5cccf00fc7b28dfc93c1a70b2880df3720209288
M.TASHFEEN <kingtashfeen@gmail.com>
2021-12-01 15:04:58 +0500

    51,use effect takes 2 parameters

--- src/components/UseEFFECT.js
+++ src/components/UseEFFECT.js
@@ -3,16 +3,22 @@ import React, {useState , useEffect} from 'react'
 function UseEFFECT() {
 
     const [count , setCount] = useState(0)
+    const [ name , setName ] = useState('')
 
     useEffect(
         ()=>{
+            console.log('updating document title')
             document.title = `You Clicked ${count} times`
-        }
+        }, [count]
     
     )
     return (
         <div>
-            <button onClick={()=> setCount(count + 1)} >Clicl {count} times</button>
+            <input type='text' 
+            value={name}
+            onChange={(e) => setName(e.target.value)}  />
+            <button onClick={()=> setCount(count + 1)} >
+                Click {count} times</button>
         </div>
     )
 }
